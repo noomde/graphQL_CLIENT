66a5f05ba9664472d45566570c7124d80e560a71
Anthon Bäckström <ab226rf@student.lnu.se>
2026-03-19 15:08:20 +0100

    All of the simple auth logic is added

--- src/js/auth/services/authActions.ts
+++ src/js/auth/services/authActions.ts
@@ -10,6 +10,14 @@ import type React from 'react';
 
 type SetUser = React.Dispatch<React.SetStateAction<DecodedUser | null>>;
 
+/**
+ * Handles the login action by sending a login mutation.
+ *
+ * @param client - The Apollo Client instance used to perform the mutation.
+ * @param credentials - The user's login credentials (username and password).
+ * @param setUser - A state setter function to update the user information in the application state.
+ * @throws Will throw an error if the login fails or if no token is received.
+ */
 export async function loginAction(
   client: ApolloClient,
   credentials: AuthCredentials,
@@ -23,9 +31,40 @@ export async function loginAction(
   const token = data?.loginUser?.token;
 
   if (!token) {
-    throw new Error('Login failed: No token received');
+    throw new Error('Login failed');
   }
 
   setToken(token);
   setUser(decodeToken(token));
+}
+
+/**
+ * Handles the register action by sending a register mutation.
+ *
+ * @param client - The Apollo Client instance used to perform the mutation.
+ * @param credentials - The user's registration credentials.
+ * @throws Will throw an error if the registration fails.
+ */
+export async function registerAction(
+  client: ApolloClient,
+  credentials: AuthCredentials,
+) {
+  const { data } = await client.mutate<RegisterMutationData>({
+    mutation: REGISTER_MUTATION,
+    variables: { input: credentials },
+  });
+
+  if (!data?.registerUser) {
+    throw new Error('Registration failed');
+  }
+}
+
+/**
+ * Handles the logout action by removing the JWT token and clearing the user state.
+ *
+ * @param setUser - A state setter function to clear the user information in the application state.
+ */
+export async function logoutAction(setUser: SetUser) {
+  removeToken();
+  setUser(null);
 }
