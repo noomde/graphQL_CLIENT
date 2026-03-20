db60e0f594aa44d72bfbfd618782d895f82ab584
Anthon Bäckström <ab226rf@student.lnu.se>
2026-03-20 10:43:01 +0100

    Added components and pages for users

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './apolloClient.ts'
+import { ApolloProvider } from '@apollo/client/react'
+import { client } from './js/generic/apollo/apolloClient.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
