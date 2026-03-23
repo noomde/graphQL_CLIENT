3a3fcccc7e0560351644775352351ee22733f87a
Anthon Bäckström <ab226rf@student.lnu.se>
2026-03-23 14:08:02 +0100

    Added scores graphql operations

--- src/js/auth/graphql/authOperations.ts
+++ src/js/auth/graphql/authOperations.ts
@@ -1,19 +1,18 @@
-import { gql } from '@apollo/client'
+import { gql } from '@apollo/client';
 
 export const REGISTER_MUTATION = gql`
-    mutation registerUser($username: String!, $password: String!) {
-        registerUser(username: $username, password: $password) {
-            id
-            username
-        }
+  mutation registerUser($username: String!, $password: String!) {
+    registerUser(username: $username, password: $password) {
+      id
+      username
     }
+  }
 `;
 
 export const LOGIN_MUTATION = gql`
-    mutation loginUser($username: String!, $password: String!) {
-        loginUser(username: $username, password: $password) {
-            token
-        }
+  mutation loginUser($username: String!, $password: String!) {
+    loginUser(username: $username, password: $password) {
+      token
     }
+  }
 `;
-
---
+++ src/js/scores/graphql/scoresOperations.ts
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const GET_SCORES_QUERY = gql`
+  query {
+    scores {
+      metascore
+      metascoreCount
+      metascoreSentiment
+      userScore
+      userScoreCount
+      userScoreSentiment
+    }
+  }
+`;
+
+export const GET_SCORE_QUERY = gql`
+  query score(gameId: ID!) {
+    score(gameId: $gameId) {
+      metascore
+      metascoreCount
+      metascoreSentiment
+      userScore
+      userScoreCount
+      userScoreSentiment
+    }
+  }
+`;
