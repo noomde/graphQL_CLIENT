45acfc500197c12626933077ea047cd1f77f5584
Anthon Bäckström <ab226rf@student.lnu.se>
2026-03-25 21:34:44 +0100

    Added the page for platforms

---
+++ src/js/platforms/pages/platformsPage.tsx
@@ -0,0 +1,11 @@
+import PlatformsComponent from '../components/platformsComponent';
+import { type JSX } from 'react';
+
+/**
+ * PlatformPage component serves as the page for all of the platforms.
+ *
+ * @returns {JSX.Element} The platform page component.
+ */
+export default function PlatformPage(): JSX.Element {
+  return <PlatformsComponent />;
+}
