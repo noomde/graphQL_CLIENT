816ff78f7b596122bf208f0e193d8fc3d01a48d4
Anthon Bäckström <ab226rf@student.lnu.se>
2026-03-30 18:26:22 +0200

    Added a score page

---
+++ src/js/scores/pages/scorePage.tsx
@@ -0,0 +1,11 @@
+import ScoreComponent from "../components/scoreComponent.tsx";
+import { type JSX } from 'react'
+
+/**
+ * ScorePage component serves as the page for a single score.
+ *
+ * @returns 
+ */
+export default function scorePage(): JSX.Element {
+    return <ScoreComponent/>
+}
