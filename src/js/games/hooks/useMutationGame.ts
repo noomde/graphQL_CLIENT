b906e351ddc4d2fb50bb90b98a0f6d18f0d08dfa
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-03 12:05:09 +0200

    Added update maptobackend but could refactor

--- src/js/games/hooks/useMutationGame.ts
+++ src/js/games/hooks/useMutationGame.ts
@@ -29,3 +29,5 @@ export function useCreateGame() {
     error,
   };
 }
+
+export 
--- src/js/generic/helpers/mapToBackend.ts
+++ src/js/generic/helpers/mapToBackend.ts
@@ -1,4 +1,5 @@
 import { type CreateGameInput } from '../../games/types/inputType.ts';
+import { type UpdateGameInput } from '../../games/types/inputType.ts';
 
 /**
  * Maps the CreateGameInput data to the format expected by the backend API.
@@ -12,4 +13,18 @@ export function mapGameInputToBackend(input: CreateGameInput) {
     genres: input.genres.join(', '),
   };
   return backendInput;
+}
+
+
+/**
+ * Maps the UpdateGameInput data to the format expected by the backend API.
+ *
+ * @param input - The input data for updating a game.
+ * @returns The mapped input data for the backend API.
+ */
+export function mapGameUpdateToBackend(input: UpdateGameInput) {
+  return {
+    ...input,
+    genres: input.genres ? input.genres.join(', ') : undefined,
+  };
 }
