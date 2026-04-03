2bdeec16342c2dd743d8d32e96139d9ed7000f04
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-03 17:30:20 +0200

    Added nestedgame component

---
+++ src/js/games/components/query/nestedGameComponent.tsx
@@ -0,0 +1,45 @@
+import { type JSX } from 'react';
+import { useNestedGame } from '../../hooks/useGame.ts';
+
+export default function NestedGameComponent({
+  id,
+}: {
+  id: number;
+}): JSX.Element {
+  const { game, loading, error } = useNestedGame(id);
+
+  // TODO update to make more less repetetive
+  if (loading) {
+    return <p>Loading game...</p>;
+  }
+
+  if (error || !game) {
+    return <p>Failed to load game, please try again</p>;
+  }
+
+  // TODO add the real rendering of data and the actual url for metacritic.
+  return (
+    <div>
+      <h2>{game.title}</h2>
+
+      <p>Release Date: {game.releaseDate}</p>
+      <p>Rating: {game.rating}</p>
+      <p>Genres: {game.genres}</p>
+      <p>Description: {game.description}</p>
+      <p>Developer: {game.developer}</p>
+      <p>Publisher: {game.publisher}</p>
+      <p>Metacritic url: {game.metacriticId}</p>
+
+      <p>
+        Platforms: {game.platforms.map((platform) => platform.name).join(', ')}
+      </p>
+
+      <p>Metascore: {game.scores?.metascore}</p>
+      <p>Metascore Count: {game.scores?.metascoreCount}</p>
+      <p>Metascore Sentiment: {game.scores?.metascoreSentiment}</p>
+      <p>UserScore: {game.scores?.userScore}</p>
+      <p>UserScore Count: {game.scores?.userScoreCount}</p>
+      <p>UserScore Sentiment: {game.scores?.userScoreSentiment}</p>
+    </div>
+  );
+}
