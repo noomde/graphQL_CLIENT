5d93f6255f21c3cad072b7869707db5dcf977edf
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-07 14:30:42 +0200

    Added gamePage

---
+++ src/js/games/pages/query/gamePage.tsx
@@ -0,0 +1,9 @@
+import GameComponent from "../../components/query/gameComponent";
+import { type JSX } from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function GamePage(): JSX.Element {
+    const { id } = useParams<{ id: string }>();
+
+    return <GameComponent id={Number(id)} />
+}
