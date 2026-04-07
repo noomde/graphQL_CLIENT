eb8543ea44f39da2f7f3ebf6c9abe4acb91d8c01
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-07 20:43:58 +0200

    Added

---
+++ src/js/games/pages/query/nestedGamesPage.tsx
@@ -0,0 +1,6 @@
+import NestedGamesComponent from '../../components/query/nestedGamesComponent.tsx';
+import { type JSX } from 'react';
+
+export default function NestedGamesPage(): JSX.Element {
+  return <NestedGamesComponent />;
+}
