fa1ae81a7d3f46df4d018546b72c05427aae968d
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-20 21:49:32 +0200

    Added client start on express server

--- server/index.ts
+++ server/index.ts
@@ -2,17 +2,35 @@ import express from 'express';
 import router from './router.ts';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+const PORT = process.env.PORT || 3000;
+const distPath = path.join(__dirname, '../dist');
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+app.use(express.static(distPath));
+
+app.get('/{*splat}', (_, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`OAuth server running on http://localhost:${PORT}`);
+  console.log(`OAuth server running on port ${PORT}`);
 });
