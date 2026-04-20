8cd2d3e9115e0b2a583988939202afa087d840da
Anthon Bäckström <ab226rf@student.lnu.se>
2026-04-20 22:28:41 +0200

    Added small oauth fix hopefully

--- server/index.ts
+++ server/index.ts
@@ -26,6 +26,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
+
+app.use('/api', (_, res) => {
+  res.status(404).json({ error: 'API route not found' });
+});
+
 app.use(express.static(distPath));
 
 app.get('/{*splat}', (_, res) => {
